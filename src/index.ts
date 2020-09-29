9f4561eceafffc48d81a26e17992b54da0dfbe58
antwills <antwills@gmail.com>
2020-09-29 11:00:13 +0100

    Reordered import statements - grouping amcharts related imports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 
-import * as am4core from "@amcharts/amcharts4/core";
 import "./style.css"; // webpack will handing this loading...
+
+import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 // import am4themes_animated from '@amcharts/amcharts4/themes/animated';
 
